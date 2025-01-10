a86561216b416d736d497537e869e48a145871c9
27kittu <cv27.one@gmail.com>
2025-01-10 17:31:41 +0530

    added pyproject.toml and other files modified

--- math_tables_py/bin/start_tables.py
+++ math_tables_py/bin/start_tables.py
@@ -2,10 +2,7 @@
 
 import os, sys
 
-sys.path.insert(0,os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
-print(sys.path)
-
-from tmath_tables_py.train import main
+from ..train import main
 
 if __name__ == '__main__': 
     main()
